bcb98c627cfa71fc0c9b3627ce517dea133ac663
Pisoth567 <pisoth887@gmail.com>
2025-08-25 16:19:04 +0700

    Hospital Module

---
+++ __manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name' : 'Hospital Management',
+    'version': '1.0.0',
+    'category' : 'Hospital',
+    'sequence':-100,
+    'author': 'Odoo Mates',
+    'summary' : "Hospital Management System",
+    'description': """Hospital Management System""",
+    'data':[
+        'Views/menu.xml'
+    ],
+    'demo': [],
+    'application':True,
+    'auto_install': False,
+    'license':'LGPL-3',
+}
---
+++ models/patient.py
@@ -0,0 +1,10 @@
+from odoo import api, fields, models
+
+class HospitalPatient(models.Model):
+    _name = 'hospital.patient'
+    _description = 'Hospital Patient'
+
+    name = fields.Char(string="Name")
+    age = fields.Integer(string="Age")
+    Gender = fields.Selection([('male', 'Male'), ('female','Female')], string="Gender")
+
