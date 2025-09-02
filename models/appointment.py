69b80b4d97fdfa059a83ec2493d07934e5da091e
Pisoth567 <pisoth887@gmail.com>
2025-09-02 17:17:58 +0700

    create appoiontment new file

---
+++ models/appointment.py
@@ -0,0 +1,12 @@
+from odoo import api, fields, models
+class HospitalAppointment(models.Model):
+    _name = 'hospital.appointment'
+    _inherit = ['mail.thread','mail.activity.mixin']
+    _description = 'Hospital Appointment'
+
+    patient_id = fields.Many2one('hospital.patient' , string="Patient")
+    gender = fields.Selection(related="patient_id.gender", readonly=False)
+    appointment_time = fields.Datetime(string="Appointment Time")
+    booking_date = fields.Date(string="Booking Date")
+
+
